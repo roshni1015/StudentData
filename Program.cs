d02ece81a2e10e58c904dc24f73a6ab4aadb16e1
roshni1015 <roshniadatrao1110@gmail.com>
2022-04-16 23:42:48 +0530

    student commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Student_Data
+{
+    public class Student
+    {
+        public void StudentData()
+        {
+            Console.WriteLine("Student Data:\nName:Roshni\nEducation:BE\nAddress:Test");
+        }
+    }
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hello World!");
+            Student student = new Student();
+            student.StudentData();
+        }
+    }
+}
